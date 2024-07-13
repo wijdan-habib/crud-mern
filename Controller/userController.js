90cfb7f948ef920c8f2bb59e4cfa195cd91cd1fe
Wijdan habib <mwijdanamir@gmail.com>
2024-07-13 13:17:31 -0700

    all api's complete

--- Controller/userController.js
+++ Controller/userController.js
@@ -15,4 +15,56 @@ const create = async (req, res) => {
     }
 };
 
-module.exports = { create };
+const getAllUser = async (req, res) => {
+    try {
+        const userData = await data.find();
+        if (!userData || userData.length === 0) {
+            return res.status(404).json({ message: "User Not Found !!" });
+        }
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+};
+
+const getUserByid = async (req,res) => {
+try {
+    const id = req.params.id;
+    const userExist = await data.findById(id);
+    if(!userExist){
+        return res.status(404).json({ message: "User Not Found !!" });
+    }
+    res.status(200).json(userExist)
+} catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+}
+}
+const updateUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const userExist = await data.findById(id);
+        if(!userExist){
+            return res.status(404).json({ message: "User Not Found !!" });
+        }
+       const updatedData = await data.findByIdAndUpdate(id, req.body,{
+            new: true
+        })
+        res.status(200).json(updatedData)
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+}
+const deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const userExist = await data.findById(id);
+        if(!userExist){
+            return res.status(404).json({ message: "User Not Found !!" });
+        }
+        await data.findByIdAndDelete(id)
+        res.status(200).json({message: "user deleted successfully"})
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+}
+module.exports = { create, getAllUser, getUserByid, updateUser, deleteUser };
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { create } = require('../Controller/userController.js');
+const { create, getAllUser, getUserByid, updateUser, deleteUser } = require('../Controller/userController.js');
 
 const route = express.Router();
 
 route.post('/route', create);
+route.get('/routes', getAllUser);
+route.get('/route/:id', getUserByid);
+route.put('/update/user/:id', updateUser);
+route.delete('/delete/user/:id', deleteUser);
 
 module.exports = route; // Correct way to export in CommonJS
